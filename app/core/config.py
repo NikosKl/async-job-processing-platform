99bb6b00674fc8e44cf57b996442c25977a3d954
NikosKl <nikos.klavi@gmail.com>
2026-08-26 22:35:49 +0300

    feat: add application settings

---
+++ app/core/config.py
@@ -0,0 +1,16 @@
+from typing import Literal
+
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+class Settings(BaseSettings):
+    app_name: str = "Async Job Processing Platform"
+    environment: Literal["development", "testing", "production"] = "development"
+    debug: bool = False
+
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        env_file_encoding="utf-8",
+        extra="ignore"
+    )
+
+settings = Settings()
--- app/main.py
+++ app/main.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI
 from app.api.router import api_router
+from app.core.config import settings
 
-app = FastAPI()
+app = FastAPI(
+    title=settings.app_name,
+    debug=settings.debug,
+)
 
 app.include_router(api_router)
