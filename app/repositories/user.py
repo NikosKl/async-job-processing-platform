e5cb9d42e569f274de2926063044bb0a32276bac
NikosKl <nikos.klavi@gmail.com>
2026-09-15 17:59:27 +0300

    feat: add user creation repository

--- app/repositories/user.py
+++ app/repositories/user.py
@@ -7,3 +7,11 @@ from app.models import User
 def get_user_by_email(db: Session, email: str) -> User | None:
     stmt = select(User).where(User.email == email)
     return db.scalars(stmt).one_or_none()
+
+
+def create_user(db: Session, email: str, hashed_password: str) -> User:
+    user = User(email=email, hashed_password=hashed_password)
+    db.add(user)
+    db.flush()
+
+    return user
--- tests/integration/repositories/test_users.py
+++ tests/integration/repositories/test_users.py
@@ -1,5 +1,8 @@
+from sqlalchemy import select
+
+from app.core.security import hash_password
 from app.models import User
-from app.repositories.user import get_user_by_email
+from app.repositories.user import create_user, get_user_by_email
 
 
 def test_get_user_by_email_success(db_session):
@@ -24,3 +27,21 @@ def test_get_user_by_email_returns_none_when_not_found(db_session):
     returned_user = get_user_by_email(db_session, email)
 
     assert returned_user is None
+
+
+def test_create_user_returns_user_with_all_values(db_session):
+    email = "user@example.com"
+    hashed_password = "fake_hashed_password"
+
+    user = create_user(db_session, email, hashed_password)
+
+    assert user.id is not None
+    assert user.email == email
+    assert user.hashed_password == hashed_password
+    assert user.is_active is True
+    assert user.created_at is not None
+
+    stmt = select(User).where(User.email == email, User.id == user.id)
+    retrieved_user = db_session.scalars(stmt).one_or_none()
+
+    assert retrieved_user is not None
